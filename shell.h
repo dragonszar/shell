d65743beb16a1937bec18fd1d537bb6c74b4174e
dragonszar <zongyang.hu@gmail.com>
2013-11-13 22:11:01 -0500

    Small Change to shell.h

--- shell.h
+++ shell.h
@@ -1,16 +1,16 @@
 //////////////////////////////////////////////////////////////////////////////////////
 //Function Prototypes
 bool is_built_in_command(char * command);   //Determines if command is a built in type
-void run_built_in_command(char * command);  //Perfoms the Built-In command
+void run_built_in_command(command command);  //Perfoms the Built-In command
 void run_user_command(char * command);      //Running user command
-void debug(struct command command)
+void debug(command command)
 
 
 
 //////////////////////////////////////////////////////////////////////////////////////
 //Struct Declarations
 
-struct command {    // represents a single command to be exec'ed
+typedef struct command {    // represents a single command to be exec'ed
   char *command;
   char *argument[];
   char *infile;
