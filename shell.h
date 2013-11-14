59caf6e90da14f31a78d9b09ca7ce6fd93bb1125
mklbtz <mklbtz@gmail.com>
2013-11-13 22:15:31 -0500

    bug fixes in shell.h

--- shell.h
+++ shell.h
@@ -3,7 +3,7 @@
 bool is_built_in_command(char * command);   //Determines if command is a built in type
 void run_built_in_command(command command);  //Perfoms the Built-In command
 void run_user_command(char * command);      //Running user command
-void debug(command command)
+void debug(command command);
 
 
 
@@ -17,9 +17,9 @@ typedef struct command {    // represents a single command to be exec'ed
   char *outfile;
 };
 
-struct Token    // represents the tokens parsed by flex
+typedef struct token    // represents the tokens parsed by flex
 {
-    int type; // 1=word, 2=string, 3=meta, 4=comment, 0=end
+    int type;  // 1=word, 2=string, 3=meta, 4=comment, 0=end
     char *string;
 };
 
