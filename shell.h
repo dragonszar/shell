13f2632cad633f71aef2aaf3c4ed7ca59715b12d
dragonszar <zongyang.hu@gmail.com>
2013-11-13 23:47:30 -0500

    complete convertion to c+

--- shell.h
+++ shell.h
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////////
 //Function Prototypes
-bool is_built_in_command(char * command);   //Determines if command is a built in type
-void run_built_in_command(char * command);  //Perfoms the Built-In command
-void run_user_command(char * command);      //Running user command
+bool is_built_in_command(string command);   //Determines if command is a built in type
+void run_built_in_command(string command);  //Perfoms the Built-In command
+void run_user_command(string command);      //Running user command
 //void debug(char * command);
 
 
