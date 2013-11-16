773f973fc1ad91da283595c4da02def114473ea3
mklbtz <mklbtz@gmail.com>
2013-11-15 21:55:10 -0500

    removed git fluff

--- shell.cpp
+++ shell.cpp
@@ -17,20 +17,14 @@ using namespace std;
 
 int main (int argc, char **argv)
 {
-<<<<<<< HEAD
-=======
-
->>>>>>> b489ac6dafaed7d7f24e5cf746f8d3aa5165c07e
   // Running the shell requires an unterminated loop to get new commands
   while( Command::quit == false )
   {
     cout << Command::prompt << " ";
 
-<<<<<<< HEAD
     // Getting Input
     string cinput;
     getline(cin, cinput);
-    // cout << cinput << endl;
 
     Command::debug = true;
 
