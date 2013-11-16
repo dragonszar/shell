b489ac6dafaed7d7f24e5cf746f8d3aa5165c07e
dragonszar <zongyang.hu@gmail.com>
2013-11-15 19:24:15 -0500

    Forgot to upload, this is the final comments

--- shell.cpp
+++ shell.cpp
@@ -18,29 +18,10 @@ using namespace std;
 int main (int argc, char **argv)
 {
 
-  // Running the shell requires an loop to always get new commands
-
   // Running the shell requires an unterminated loop to get new commands
-
   while( Command::quit == false )
   {
     cout << Command::prompt << " ";
-    list<string> args1,args;
-
-    // Getting Input
-    string cinput;
-    cin >> cinput;
-
-    args.push_back("on");
-
-    args1.push_back("-a");
-    args1.push_back("-l");
-
-
-    Command example("debug", args, "", "");
-    example.execute();
-    Command example1("/bin/ls", args1, "","");///home/student/Downloads/shell/progoutput.txt");
-    example1.execute();
 
     /*
     list<Command> commands = parseForCommands(cinput);
@@ -55,8 +36,6 @@ int main (int argc, char **argv)
     }
 
     */
-
-
   }
 }
 
